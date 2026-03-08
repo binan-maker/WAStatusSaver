d548796a5317eef8f20c65acb03c0a5f9819d490
Binan <ahmedsameerbinan2@gmail.com>
2026-03-08 12:38:53 +0530

    Video a little bit image and video updated but not fixed as thinked but working

--- constants/admob.ts
+++ constants/admob.ts
@@ -2,20 +2,20 @@ import { Platform } from 'react-native';
 
 export const AD_UNIT_IDS = {
   BANNER: Platform.OS === 'android'
-    ? 'ca-app-pub-7313264435552019/3364238531'
-    : 'ca-app-pub-7313264435552019/3364238531',
+    ? 'ca-app-pub-8785278012936203/9814176453'
+    : 'ca-app-pub-8785278012936203/9814176453',
 
   INTERSTITIAL: Platform.OS === 'android'
-    ? 'ca-app-pub-7313264435552019/9990879231'
-    : 'ca-app-pub-7313264435552019/9990879231',
+    ? 'ca-app-pub-8785278012936203/8006746126'
+    : 'ca-app-pub-8785278012936203/8006746126',
 
   APP_OPEN: Platform.OS === 'android'
-    ? 'ca-app-pub-7313264435552019/7350702672'
-    : 'ca-app-pub-7313264435552019/7350702672',
+    ? 'ca-app-pub-8785278012936203/8469780552'
+    : 'ca-app-pub-8785278012936203/8469780552',
 
   NATIVE: Platform.OS === 'android'
-    ? 'ca-app-pub-7313264435552019/8499298440'
-    : 'ca-app-pub-7313264435552019/8499298440',
+    ? 'ca-app-pub-8785278012936203/5380582780'
+    : 'ca-app-pub-8785278012936203/5380582780',
 };
 
 export const VIDEO_AD_FREQUENCY = 3;
